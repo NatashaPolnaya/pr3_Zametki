ec971f34de59cab7bcf25bca6bb37101494878a4
NatashaPolnaya <natashapolnaya@yandex.ru>
2021-02-03 14:40:19 +0300

    Настройка страницы Заметок

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const port = 2000;
+
+app.set("view engine", "hbs");
+
+app.listen(port, () => {
+    console.log(`App is running on http://localhost:${port}/`);
+});
+
+app.get("/", (request, response) => {
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    response.render("index", model);
+});
+
+app.get("/", (request, response) => {
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    response.render("index", model);
+});
+
+app.get("/*", (request, response) => {
+    response.redirect("/");
+});
