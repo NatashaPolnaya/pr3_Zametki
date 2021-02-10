3f51fb8fa57b83f43fe8d70a4bcd42165aa366e5
NatashaPolnaya <natashapolnaya@yandex.ru>
2021-02-10 14:39:59 +0300

    Сделана админка(сохр.,удал.,ред.) и авторизация

--- app.js
+++ app.js
@@ -1,68 +1,118 @@
 const express = require("express");
 const fs = require("fs");
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 2000;
+const auth = {
+    login: "NatashaPolnaya",
+    pass: "2104"
+    }
 
 app.set("view engine", "hbs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/admin",(request, response, next) => {
+    const user = request.query;
+    
+    if (!user || user.login != auth.login || user.pass != auth.pass) {
+        response.header({"Content-Type": "text/html; charset=utf-8"});
+        response.status(401);
+        response.send("Ошибка авторизации");
+    }
+    next();
+});
+
+
 app.listen(port, () => {
-console.log(`App is running on http://localhost:${port}/`);
+    console.log(`App is running on http://localhost:${port}/`);
 });
 
 app.get("/", (request, response) => {
-const file = fs.readFileSync("./data/notes.json");
+    const file = fs.readFileSync("./data/notes.json");
 
-const model = JSON.parse(file);
-response.render("index", model);
+    const model = JSON.parse(file);
+    response.render("index", model);
 });
 
 app.get("/info/:id", (request, response) => {
-const id = request.params.id;
-const file = fs.readFileSync("./data/notes.json");
-const model = JSON.parse(file);
-const notes = model.notes.filter(x => x.id == id);
-
-if (notes.length < 1) response.redirect("/");
-
-response.render("info", notes[0]);
-});
-
-app.get("/admin/edit/:id", (request, response) => {
     const id = request.params.id;
     const file = fs.readFileSync("./data/notes.json");
     const model = JSON.parse(file);
     const notes = model.notes.filter(x => x.id == id);
-    
-    let note = {};
-    if (notes.length > 0) note = notes[0];
-    
-    response.render("info", note);
+
+    if (notes.length < 1) response.redirect("/");
+
+    response.render("info", notes[0]);
 });
-    
-app.post("/admin/edit", (request, response) => {
-    const editInfo = request.body;
-    if (!editInfo) response.redirect("/");
-    
+
+app.get("/admin/edit/:id", (request, response) => {
+    const id = request.params.id;
+
     const file = fs.readFileSync("./data/notes.json");
     const model = JSON.parse(file);
     const notes = model.notes.filter(x => x.id == id);
-    
-    if (notes.length < 1) {
-        const note = notes[0];
-    
+
+    let note = {};
+    if (notes.length > 0) note = notes[0];
+
+    response.render("edit-info", note);
+});
+
+app.get("/admin/add", (request, response) => {
+    const note = {};
+
+    response.render("edit-info", note);
+});
+
+app.post("/admin/edit", (request, response) => {
+    const editInfo = request.body;
+    if (!editInfo) response.redirect("/");
+
+    editInfo.id = Number(editInfo.id);
+
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    const notes = model.notes.filter(x => x.id == editInfo.id);
+    let note = {};
+    if (notes.length > 0) {
+        note = notes[0];
+
         note.title = editInfo.title;
         note.text = editInfo.text;
     } else {
-        note.id = notes[notes.length - 1].id + 1;
-        notes.push(note);
+        note.id = model && model.length
+            ? model[model.length - 1].id + 1
+            : 1;
+        note.title = editInfo.title;
+        note.text = editInfo.text;
+
+        model.notes.push(note);
     }
-    
+
+    const json = JSON.stringify(model);
+    fs.writeFileSync("./data/notes.json", json);
+
     response.redirect("/");
 });
 
-
+app.get("/admin/delete/:id", (request, response) => {
+    let id = request.params.id;
+    
+    if (!id) response.redirect("/");
+    id = Number(id);
+    
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    model.notes = model.notes.filter(x => x.id != id);
+    
+    const json = JSON.stringify(model);
+    fs.writeFileSync("./data/notes.json", json);
+    
+    response.redirect("/"); 
+})
 
 app.get("/*", (request, response) => {
-response.redirect("/");
+    response.redirect("/");
 }); 
